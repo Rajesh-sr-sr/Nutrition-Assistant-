b8ca0a5c4855d9c8e6e3bf82fd822530a47982d7
Battula Hareesh <238x1a0510@khitguntur.ac.in>
2026-07-03 17:27:31 +0530

    Add user routes for login and registration

---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { userLogin, userRegister } = require("../controllers/userController.js");
+
+const router = express.Router();
+
+router.post('/login', userLogin);
+router.post('/register', userRegister);
+
+module.exports = router;
